a03bd79c752e712cb451dfcf1dc17a6fa7fd6bb5
Connor Brown <connorbrown1996@gmail.com>
2026-05-13 09:42:41 -0500

    Update httr2.R

--- R/httr2.R
+++ R/httr2.R
@@ -1,7 +1,7 @@
 http_req_throttle <- function(req) {
   httr2::req_throttle(
     req,
-    capacity = 30,
+    capacity = 120,
     fill_time_s = 60
   )
 }
@@ -11,7 +11,7 @@ http_req_perform_parallel <- function(
   ...,
   on_error = c("stop", "return", "continue"),
   progress = FALSE,
-  max_active = 4
+  max_active = 8
 ) {
   on_error <- match.arg(on_error)
   reqs <- lapply(reqs, http_req_throttle)
